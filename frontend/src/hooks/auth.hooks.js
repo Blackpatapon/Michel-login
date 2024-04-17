2e53db69f85a1e1e62980a8e5909dc2096d8d8d0
Luis Angel Hidalgo Vazquez <135643171+Blackpatapon@users.noreply.github.com>
2024-04-16 22:47:50 -0600

    Delete frontend directory

--- frontend/src/hooks/auth.hooks.js
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-import { UserContext } from "../context/auth.context";
-
-export function useAuth(){
-    const context = useContext(UserContext)
-    return context
-}
